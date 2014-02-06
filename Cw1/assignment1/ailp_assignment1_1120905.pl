3c1a6dfbd0eff6ecf8828a3dd66df55893e73725
Dominic Moylett <dm1905@bristol.ac.uk>
2014-02-06 01:48:15 +0000

    Added solutions for q5.

--- Cw1/assignment1/ailp_assignment1_1120905.pl
+++ Cw1/assignment1/ailp_assignment1_1120905.pl
@@ -1,8 +1,8 @@
 candidate_number(1120905).
 
-q1('ailp_start_position(p(X,Y)).').
+q1(ailp_start_position(p(X,Y)).).
 
-q2a('new_pos(p(1,1), e, p(X1, Y1)).').
+q2a(new_pos(p(1,1), e, p(X1, Y1)).).
 
 q2b(19).
 
@@ -15,3 +15,16 @@ q4b([p(4,2),p(4,1),p(3,1),p(3,2),p(3,3),p(4,3),p(4,4),p(3,4),p(2,4),p(2,3),p(2,2
 q4c([p(1,4),p(1,3),p(1,2),p(1,1),p(2,1),p(3,1),p(4,1),p(4,2),p(3,2),p(3,3),p(4,3),p(4,4),p(3,4),p(2,4),p(2,3),p(2,2)]).
 
 q4d([p(4,1),p(4,2),p(4,3),p(4,4),p(3,4),p(3,3),p(3,2),p(3,1),p(2,1),p(1,1),p(1,2),p(1,3),p(1,4),p(2,4),p(2,3),p(2,2)]).
+
+q5_corner_move :- ailp_start_position(p(X,Y)),
+    ailp_show_move(p(X,Y), p(1,1)),
+    ailp_show_move(p(1,1), p(1,4)),
+    ailp_show_move(p(1,4), p(4,4)),
+    ailp_show_move(p(4,4), p(4,1)).
+
+q5_corner_move2 :- ailp_start_position(p(X,Y)),
+    ailp_grid_size(N),
+    ailp_show_move(p(X,Y), p(1,1)),
+    ailp_show_move(p(1,1), p(1,N)),
+    ailp_show_move(p(1,N), p(N,N)),
+    ailp_show_move(p(N,N), p(N,1)).
