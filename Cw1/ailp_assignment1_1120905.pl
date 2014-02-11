97fb226330b92aeef8d21f0093c1af05d925db5d
Dominic Moylett <dm1905@bristol.ac.uk>
2014-02-11 17:00:21 +0000

    Added overflow property to CSS file. Improved various answers.

--- Cw1/ailp_assignment1_1120905.pl
+++ Cw1/ailp_assignment1_1120905.pl
@@ -1,37 +1,38 @@
 candidate_number(1120905).
 
-q1(ailp_start_position(p(X,Y)).).
+q1(ailp_start_position(P)).
 
-q2a(new_pos(p(1,1), e, p(X1, Y1)).).
+q2a(new_pos(p(1,1), e, P)).
 
 q2b(19).
 
 q3([s,e,w,n]).
 
-q4a([p(1,4),p(1,3),p(1,2),p(1,1),p(2,1),p(3,1),p(4,1),p(4,2),p(3,2),p(3,3),p(4,3),p(4,4),p(3,4),p(2,4),p(2,3),p(2,2)]).
+q4a([p(2, 2), p(2, 3), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(3, 2), p(4, 2), p(4, 1), p(3, 1), p(2, 1), p(1, 1), p(1, 2), p(1, 3), p(1, 4)]).
 
-q4b([p(4,2),p(4,1),p(3,1),p(3,2),p(3,3),p(4,3),p(4,4),p(3,4),p(2,4),p(2,3),p(2,2)]).
+q4b([p(2,2),p(2,3),p(2,4),p(3,4),p(4,4),p(4,3),p(3,3),p(3,2),p(3,1),p(4,1),p(4,2)]).
 
-q4c([p(1,4),p(1,3),p(1,2),p(1,1),p(2,1),p(3,1),p(4,1),p(4,2),p(3,2),p(3,3),p(4,3),p(4,4),p(3,4),p(2,4),p(2,3),p(2,2)]).
+q4c([p(2, 2), p(2, 3), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(3, 2), p(4, 2), p(4, 1), p(3, 1), p(2, 1), p(1, 1), p(1, 2), p(1, 3), p(1, 4)]).
 
-q4d([p(4,1),p(4,2),p(4,3),p(4,4),p(3,4),p(3,3),p(3,2),p(3,1),p(2,1),p(1,1),p(1,2),p(1,3),p(1,4),p(2,4),p(2,3),p(2,2)]).
+q4d([p(2,2),p(2,3),p(2,4),p(1,4),p(1,3),p(1,2),p(1,1),p(2,1),p(3,1),p(3,2),p(3,3),p(3,4),p(4,4),p(4,3),p(4,2),p(4,1)]).
 
-q5_corner_move :- ailp_start_position(p(X,Y)),
-    ailp_show_move(p(X,Y), p(1,1)),
+q5_corner_move :- ailp_start_position(P),
+    ailp_show_move(P, p(1,1)),
     ailp_show_move(p(1,1), p(1,4)),
     ailp_show_move(p(1,4), p(4,4)),
     ailp_show_move(p(4,4), p(4,1)).
 
-q5_corner_move2 :- ailp_start_position(p(X,Y)),
+q5_corner_move2 :- ailp_start_position(P),
     ailp_grid_size(N),
-    ailp_show_move(p(X,Y), p(1,1)),
+    ailp_show_move(P, p(1,1)),
     ailp_show_move(p(1,1), p(1,N)),
     ailp_show_move(p(1,N), p(N,N)),
     ailp_show_move(p(N,N), p(N,1)).
 
 q6_spiral(R) :- ailp_start_position(P),
 	ailp_show_move(P, p(1,1)),
-	q6_spiral(p(1,1), e, [p(1,1)], R),
+	q6_spiral(p(1,1), e, [p(1,1)], X),
+	reverse(X, R),
 	!.
 
 q6_spiral(_, _, R, R) :- complete(R).
